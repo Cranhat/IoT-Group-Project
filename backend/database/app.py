e9d8dc742f4147589fc94a9c4a621c8f2c3b57bc
Cranhat <cyprian.burdzy@gmail.com>
2026-04-16 09:17:36 +0200

    added docker container

--- backend/database/app.py
+++ backend/database/app.py
@@ -2,10 +2,17 @@ from backend.database.src.database import *
 from backend.database.src.db_init import * 
 from backend.database.src.db_insert import *
 import uvicorn
+import os
 
-db = Database()
+db = Database(
+    host=os.getenv("DB_HOST", "localhost"),
+    dbname=os.getenv("DB_NAME", "postgres"),
+    user=os.getenv("DB_USER", "postgres"),
+    password=os.getenv("DB_PASSWORD", "postgres"),
+    port=int(os.getenv("DB_PORT", "5432")),
+)
 db.initializeTables()
 app = db.app
 
 def main():
-    uvicorn.run("backend.app.main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("backend.database.app:app", host="0.0.0.0", port=8000, reload=False)
