7d435cc2e38872b2b1b06f6d1ace47fe876cf516
Natomkul <natomkula@gmail.com>
2026-05-01 21:00:51 +0200

    feat: add TLS 1.3 with encryption only

--- communication/server.py
+++ communication/server.py
@@ -1,9 +1,19 @@
 import socket
+import ssl
 
 HOST = "0.0.0.0"
 PORT = 3000
 BACKLOG = 5
 BUF_SIZE = 1024
+
+ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+ctx.minimum_version = ssl.TLSVersion.TLSv1_3
+ctx.maximum_version = ssl.TLSVersion.TLSv1_3
+
+ctx.load_cert_chain("server.crt", "server.key")
+
+ctx.load_verify_locations("ca.pem")
+ctx.verify_mode = ssl.CERT_NONE
 
 def server():
     try:
@@ -18,6 +28,7 @@ def server():
         print("server: waiting to connect...")
 
         cfd, addr = sfd.accept()
+        cfd = ctx.wrap_socket(cfd, server_side=True)
         print(f"Accepted socket fd = {cfd.fileno()}")
         print(f"Connection from {addr}")
 
